2a4f01fd1715e0e6bd340f5f481bf27baa9049f1
Jiahao Guo <frantic1048@users.noreply.github.com>
2016-06-08 00:59:20 +0800

    Create autoshenbao.user.js

---
+++ autoshenbao.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        autoshenbao
+// @namespace   autoshenbao
+// @include     */quanquan6.com/adminmetro/declaration/ShowApplySubject.aspx*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+window.addEventListener('load', () => {
+  document.querySelector('#lbnApply').click();
+})
